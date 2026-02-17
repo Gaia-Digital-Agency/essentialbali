820bdba6a179de40f12cdadc852d9ec157153e5f
Reza Firmansyah <erzethones.inc@gmail.com>
2026-02-17 17:29:24 +0800

    wip (button) : add new button variant

--- frontend/src/components/front/Button.tsx
+++ frontend/src/components/front/Button.tsx
@@ -1,63 +1,107 @@
-import React, {useRef} from "react"
-import { useGSAP } from "@gsap/react"
-import gsap from "gsap"
-import { Link } from "react-router"
-import { ButtonChevron, ButtonChevronBorder } from "../../icons"
+import React, { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { Link } from "react-router";
+import { ButtonChevron, ButtonChevronBorder } from "../../icons";
 
 type ButtonProps = {
-    text: string
-    link?: string,
-    bigger?: boolean,
-    onClick?: (e: React.MouseEvent<HTMLDivElement>) => void,
-    borderOnly?: boolean,
-    uppercase?: boolean
-}
+  text: string;
+  link?: string;
+  bigger?: boolean;
+  onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
+  borderOnly?: boolean;
+  uppercase?: boolean;
+  type?: string;
+};
 
-const Button: React.FC<ButtonProps> = ({text, link, bigger = false, onClick = () => {}, borderOnly = false, uppercase = false}) => {
-    const buttonRef = useRef<HTMLDivElement | null>(null)
+const Button: React.FC<ButtonProps> = ({
+  text,
+  link,
+  bigger = false,
+  onClick = () => {},
+  uppercase = false,
+  type = "primary",
+  borderOnly = false,
+}) => {
+  
+  const buttonRef = useRef<HTMLDivElement | null>(null);
 
-    const {contextSafe} = useGSAP({scope: buttonRef})
+  const { contextSafe } = useGSAP({ scope: buttonRef });
 
-    const mouseEnterHandler = contextSafe(() => {
-        if(!buttonRef.current) return
-        const icon = buttonRef.current.querySelector('.icon')
-        gsap.to(icon, {
-            width: 'auto',
-            duration: .2
-        })
-    })
+  const mouseEnterHandler = contextSafe(() => {
+    if (!buttonRef.current) return;
+    const icon = buttonRef.current.querySelector(".icon");
+    gsap.to(icon, {
+      width: "auto",
+      duration: 0.2,
+    });
+  });
 
-    const mouseLeaveHandler = contextSafe(() => {
-        if(!buttonRef.current) return
-        const icon = buttonRef.current.querySelector('.icon')
-        gsap.to(icon, {
-            width: '0px',
-            duration: .2
-        })
-    })
+  const mouseLeaveHandler = contextSafe(() => {
+    if (!buttonRef.current) return;
+    const icon = buttonRef.current.querySelector(".icon");
+    gsap.to(icon, {
+      width: "0px",
+      duration: 0.2,
+    });
+  });
 
-    const renderSVG = () => {
-        if(borderOnly) return <ButtonChevronBorder />
-        return <ButtonChevron />
-    }
+  const renderSVG = () => {
+    // if(borderOnly) return <ButtonChevronBorder />
+    return <ButtonChevron />;
+  };
 
-    const ButtonElement = () => {
-        return (
-            <div className={`button md:px-8 px-4 inline-flex text-front-body font-medium cursor-pointer ${uppercase ? 'uppercase ' : ''}${bigger ? 'py-4' : 'py-3'} ${borderOnly ? 'border border-front-red text-front-red' : 'bg-front-red text-white'}`} onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler} ref={buttonRef} onClick={onClick}>
-                {text}
-                <div className="icon overflow-hidden" style={{width: '0'}}>
-                    <div className="inner pl-2">
-                        {renderSVG()}
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  const PrimaryButtonElement = () => {
+    return (
+      <div
+        className={`button md:px-8 px-4 inline-flex text-front-body font-light cursor-pointer 
+                            rounded-sm border !border-color-navy text-front-navy
+                            hover:bg-front-navy hover:text-front-icewhite hover:border-front-navy
+                            transition-all duration-300 ease-in-out
+                            ${uppercase ? "uppercase " : ""}
+                            ${bigger ? "py-4" : "py-3"} `}
+        ref={buttonRef}
+        onClick={onClick}
+      >
+        {text}
+      </div>
+    );
+  };
 
-    if(link) {
-        return <Link to={link}><ButtonElement /></Link>
-    }
-    return <ButtonElement />
-}
+  const SecondaryButtonElement = () => {
+      return (
+          <div className={`button md:px-8 px-4 inline-flex text-front-body font-light cursor-pointer
+                          rounded-[5px] border border-front-white text-front-icewhite
+                          ${uppercase ? 'uppercase ' : ''}
+                          ${bigger ? 'py-4' : 'py-3'}`}
+              onMouseEnter={mouseEnterHandler}
+              onMouseLeave={mouseLeaveHandler}
+              ref={buttonRef}
+              onClick={onClick}>
+              {text}
+              <div className="icon overflow-hidden" style={{width: '0', color: 'white'}}>
+                  <div className="inner pl-2">
+                      {renderSVG()}
+                  </div>
+              </div>
+          </div>
+      )
+  }
 
-export default Button
+
+  let ComponentButton = SecondaryButtonElement;
+  if (type === "primary") {
+    ComponentButton = PrimaryButtonElement;
+  }
+
+  if (link) {
+    return (
+      <Link to={link}>
+        <ComponentButton />
+      </Link>
+    );
+  }
+  return <ComponentButton />;
+};
+
+export default Button;
